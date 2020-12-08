2c32daf91bac1d318bdbf27a7dae408a7c655839
xyf <82054510@qq.com>
2020-12-08 13:41:01 +0800

    add like

--- article/list_views.py
+++ article/list_views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 from article.models import ArticlePost
 
 
 def article_titles(request, username=None):
-    global userinfo, user
     if username:
         user = User.objects.get(username=username)
         articles_title = ArticlePost.objects.filter(author=user)
@@ -37,4 +40,23 @@ def article_titles(request, username=None):
 
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
-    return render(request, "article/list/article_content.html", {"article:article"})
+    return render(request, "article/list/article_content.html", {"article": article})
+
+
+@csrf_exempt
+@require_POST
+@login_required(login_url="/account/login/")
+def like_article(request):
+    article_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if article_id and action:
+        try:
+            article = ArticlePost.objects.get(id=article_id)
+            if action == "like":
+                article.users_like.add(request.user)
+                return HttpResponse("1")
+            else:
+                article.users_like.remove(request.user)
+                return HttpResponse("2")
+        except:
+            return HttpResponse("no")
